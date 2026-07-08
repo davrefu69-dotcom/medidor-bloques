bb5a816bb05fa02b9a1bd800572fe5b3be692fc8
davrefu69-dotcom <davrefu69@gmail.com>
2026-07-08 20:54:50 +0200

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    RADIO_PUNTO: 50, // Toque maximizado anti-falla
+    LOTE_HISTORIAL: [],
+    PUNTOS_TACTILES: [
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 }
+    ],
+    PUNTO_SELECCIONADO: null,
+    IMAGEN_ORIGINAL: new Image()
+};
